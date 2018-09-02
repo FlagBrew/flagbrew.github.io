1a9d7f5d3469e7c9eb4df5cef36ff5ea4b6eb7a7
Allen Lydiard <allenlydiard@gmail.com>
2018-09-02 20:35:28 -0300

    Major design overhaul complete We're also using a caching server now for some data on the page so that users don't accidently get rate limited by GitHub's API

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,14 @@
+// borrowed from https://stackoverflow.com/questions/30106476/using-javascripts-atob-to-decode-base64-doesnt-properly-decode-utf-8-strings/30106551#30106551
+var cacheServer = "https://cachebox.fm1337.com"
+
+
+function b64DecodeUnicode(str) {
+    return decodeURIComponent(Array.prototype.map.call(atob(str), function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
+    }).join(''))
+}
+
+
 function fileSize(size) {
     var i = size == 0 ? 0 : Math.floor( Math.log(size) / Math.log(1024) );
     return ( size / Math.pow(1024, i) ).toFixed(2) * 1 + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
@@ -9,64 +20,67 @@ function qrCodeGoogleApi(link) {
 
 
 function getData(repo) {
-    $.getJSON("https://api.github.com/repos/FlagBrew/" + repo +"/readme", function (data) {
-        $("#readme"+ repo +"Id").attr("href", data['_links']['html'])
-      });
-
-    $.getJSON("https://api.github.com/repos/FlagBrew/" + repo +"/releases", function (data) {
-        if ($.isArray(data) && data.length) {
-          // release title
-          document.getElementById("latestVersion" + repo).innerHTML = "Latest release: " + data[0].tag_name + " (" + data[0].published_at.substring(0, 10) + ")";
+    
+    $.getJSON(cacheServer + "/api/repos/" + repo, function (data) {
+        // set the README link
+        $("#readme" + repo + "Id").attr("href", data['readme']['_links']['html'])
+        // Get the release data
+        if ($.isArray(data['releases']) && data['releases'].length > 0) {
+            // release title
+            document.getElementById("latestVersion" + repo).innerHTML = "<li class='list-group-item'>Latest release: " + data['releases'][0].tag_name + " (" + data['releases'][0].published_at.substring(0, 10) + ")</li>";
+    
+            for (var i in data['releases'][0]["assets"]) {
+              let asset = data['releases'][0]["assets"][i];
+                $("#latestVersion" + repo).append('<li class="list-group-item">'
+                    + (asset.name.endsWith(".cia") ? ' <button class="btn btn-success" id="qr-' + repo + '"><i class="fas fa-qrcode fa-lg"></i></button> ' : '')
+                    + '<b><a href="' + asset.browser_download_url + '">' + asset.name + '</a></b>'
+                    + '(' + fileSize(asset.size) + ')'
+                    + ' Downloaded <b>' + asset.download_count + '</b> times</p>'
+                    + '</li>');
   
-          for (var i in data[0]["assets"]) {
-            let asset = data[0]["assets"][i];
-            $("#latest" + repo).append('<li>'
-            + '<b><a href="' + asset.browser_download_url + '">' + asset.name + '</a></b>'
-            + ' (' + fileSize(asset.size) + ')'
-            + (asset.name.endsWith(".cia") ? ' <button id="qr-' + repo + '"><i class="fas fa-qrcode fa-2x"></i></button> ' : '')
-            + '</li>'
-            + 'Downloaded <b>' + asset.download_count + '</b> times</p>');
-
-            // if we have a cia, we need to attach an onclick listener
-            if(asset.name.endsWith(".cia")){
-                $("#qr-" + repo).click(function(){
-                    swal({
-                        title: repo,
-                        text: 'Scan the QR Code with QRaken to install!',
-                        imageUrl: qrCodeGoogleApi(asset.browser_download_url),
-                        imageAlt: repo + " QR Code",
-                        animation: true
-                      })
-                })
+              // if we have a cia, we need to attach an onclick listener
+              if(asset.name.endsWith(".cia")){
+                  $("#qr-" + repo).click(function(){
+                      swal({
+                          title: repo,
+                          text: 'Scan the QR Code with QRaken to install!',
+                          imageUrl: qrCodeGoogleApi(asset.browser_download_url),
+                          imageAlt: repo + " QR Code",
+                          animation: true
+                        })
+                  })
+              }
             }
+            // source code
+            $("#latestVersion" + repo).append('<li class="list-group-item"><b><a href="' + data['releases'][0].zipball_url + '">Source code</a></b> for ' + data['releases'][0].tag_name + '</li>');
+          } else {
+            $("#latestVersion" + repo).append("<p class='card-text strong'>No Releases Available</p>");
           }
-          // source code
-          $("#latest" + repo).append('<li><b><a href="' + data[0].zipball_url + '">Source code</a></b> for ' + data[0].tag_name + '</li>');
-        } else {
-          document.getElementById("latestVersion" + repo).style.display = "none";
-          document.getElementById("latest" + repo).style.display = "none";
-        }
-      });
-}
-
-
-function loadProjects() {
-    // loop through projects
-    $("#projects").find('div').each(function(i, project){
-        getData(project.id.replace("project-", ""))
     })
 }
+
+
+function loadProjects(){
+            // loop through projects
+            $("div[id*='project-']").each(function(i, project){
+                getData(project.id.replace("project-", ""))
+            })
+    }
 if ($("#projects").length > 0) {
-    loadProjects();
+    $("#page-info").append("Check out our projects below or visit us on github!")
+    loadProjects()
 }
 
-function projectInfo() {
+
+function projectInfo(){
     let params = (new URL(location)).searchParams;
     let project = params.get('p');
     // if no project is specified, redirect back to the homepage
     if (project == ""){
         window.location.replace("/");
     }
+    // set the page info
+    $("#page-info").append("Check out " + project + "'s README below or visit us on GitHub!")
     // set the title
     document.title = project + " | FlagBrew"
     // clear the placeholder stuff
@@ -78,20 +92,22 @@ function projectInfo() {
     + '<img src="https://img.shields.io/github/downloads/FlagBrew/'+ project +'/total.svg"> '
     + '<img src="https://img.shields.io/github/stars/FlagBrew/' + project + '.svg">'
     + '<br />'
-    + "<a href ='https://github.com/flagbrew/"+ project +"'>View on github!</a><hr />")
+    + "<a href ='https://github.com/flagbrew/"+ project +"'><button class='btn btn-sm btn-success'>View on <i class='fab fa-github'></i> Github</button></a><hr />")
     // Next we need to get some repo data
     // TODO: Handle release data on the project page.
     // $.getJSON("https://api.github.com/repos/FlagBrew/"+ project +"/releases", function (data) {
 
     // });
     // Now we need to get the README data from github
-    $.getJSON("https://api.github.com/repos/FlagBrew/" + project +"/readme", function (data) {
+    $.getJSON(cacheServer + "/api/repos/" + project, function (data) {
         let converter = new showdown.Converter({tables: true})
-        content.append(converter.makeHtml(window.atob(data.content)))
+        content.append(converter.makeHtml(b64DecodeUnicode(data['readme'].content)))
       });
 
 }
 
 if ($("#project-content").length > 0){
-    projectInfo();
+    projectInfo()
 }
+
+
--- index.md
+++ index.md
@@ -1,31 +1,51 @@
+--- 
+layout: default 
 ---
-layout: default
----
-<div id="projects">
-{% assign sorted_repositories = site.github.public_repositories | sort: "stargazers_count" | reverse %}
-{% for repository in sorted_repositories %}
-  {% if repository.fork == false %}
-  {% unless repository.name == "EventsGalleryPacker" or repository.name == "flagbrew.github.io" %}
-  <div id="project-{{repository.name}}">
-  <h2><a href="/project?p={{repository.name}}">{{repository.name}}</a> <img src="https://img.shields.io/github/downloads/FlagBrew/{{repository.name}}/total.svg"> <img src="https://img.shields.io/github/stars/FlagBrew/{{repository.name}}.svg"></h2>
-  {% if repository.description %}
-  <h3>{{repository.description}}</h3>
-  {% else %}
-  No description
-  {% endif %}
-
-  <ul>
-    <li><b>Readme:</b> <a id="readme{{repository.name}}Id" href="README.md">README.md</a></li>
-    <li><b>Language:</b> {{repository.language}}</li>
-    <li><b>License:</b> {{repository.license.name}}</li>
-    <li><b>Last updated:</b> {{repository.updated_at}}</li>
-  </ul>
-  <h4 id="latestVersion{{repository.name}}">Latest release</h4>
-  <ul id="latest{{repository.name}}">
-  </ul>
-  </div>
-  <hr>
-  {% endunless %}
-  {% endif %}
-{% endfor %}
-</div>
+<div id="projects" class="row">
+    <!-- start of projects -->
+    {% assign sorted_repositories = site.github.public_repositories | sort: "stargazers_count" | reverse %} {% for repository
+    in sorted_repositories %} {% if repository.fork == false %} {% unless repository.name == "EventsGalleryPacker" or repository.name
+    == "flagbrew.github.io" or repository.name == "CacheBox" %}
+    <div class="col-md-4 d-flex align-items-stretch">
+        <!-- Start of column -->
+        <div class="card" id="project-{{repository.name}}" style="margin-bottom: 1rem;">
+            <!-- Start of card -->
+            <div class="card-body">
+                <!-- Start of card body -->
+                <h5 class="card-title"><a href="/project?p={{repository.name}}">{{repository.name}}</a><br>
+                    <img src="https://img.shields.io/github/downloads/FlagBrew/{{repository.name}}/total.svg">
+                    <img src="https://img.shields.io/github/stars/FlagBrew/{{repository.name}}.svg">
+                </h5>
+                {% if repository.description %}
+                <p class="card-text">{{repository.description}}</p>
+                {% else %}
+                <p class="card-text">No description</p> {% endif %}
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">
+                        <b>Readme:</b>
+                        <a id="readme{{repository.name}}Id" href="README.md">README.md</a>
+                    </li>
+                    <li class="list-group-item">
+                        <b>Language:</b> {{repository.language}}</li>
+                    <li class="list-group-item">
+                        <b>License:</b> {{repository.license.name}}</li>
+                    <li class="list-group-item">
+                        <b>Last updated:</b> {{repository.updated_at}}</li>
+                    <li class="list-group-item">
+                        <b style="cursor: pointer;" data-toggle="collapse" data-target="#versionShow-{{repository.name}}" aria-expanded="false" aria-controls="versionShow-{{repository.name}}">Latest
+                            Version <i class="fas fa-chevron-down"></i></b>
+                    </li>
+                </ul>
+                <div class="collapse" id="versionShow-{{repository.name}}">
+                    <ul class="list-group list-group-flush" id="latestVersion{{repository.name}}">
+                    </ul>
+                </div>
+            </div> <!-- End of card body -->
+            <div class="card-footer">
+                <small class="text-muted">Updated at 
+                    {{repository.updated_at | date: "%Y-%m-%d %H:%M"}}</small>
+            </div>
+        </div> <!-- End of Column -->
+    </div> <!-- End of card -->
+    {% endunless %} {% endif %} {% endfor %}
+</div> <!-- End of row -->
